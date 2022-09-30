5f29400e93f84873d495c9e53b65712affcbf338
OrengYee <orangekoucha@naver.com>
2022-09-30 17:55:47 +0900

    2022-09-30 쇼핑몰 클론코딩하기

---
+++ src/main/java/com/stussy/stussyclone20220930eunji/StussyClone20220930EunjiApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220930eunji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220930EunjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220930EunjiApplication.class, args);
+	}
+
+}
